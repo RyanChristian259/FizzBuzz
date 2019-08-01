8f41c4d2857ee2392f2425eee82269746511c5b1
Ryan Ciecalone <ryan@ryanchristianmedia.com>
2019-08-01 09:50:21 -0600

    Update Script.js
    Add space between comment lines. Change `var` to `let`.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//Fizz Buzz - outputs numbers to console from 1 to 100.
-//Numbers divisible by 3 output "Fizz", numbers divisible by 5 output "Buzz".
-//Numbers divisible by 3 and 5 output "Fizz Buzz"
+// Fizz Buzz - outputs numbers to console from 1 to 100.
+// Numbers divisible by 3 output "Fizz", numbers divisible by 5 output "Buzz".
+// Numbers divisible by 3 and 5 output "Fizz Buzz"
 
-for (var num = 1; num <= 100; num ++){
+for (let num = 1; num <= 100; num ++){
   if (num % 3 === 0 && num % 5 === 0){
     console.log("Fizz Buzz")
   }
